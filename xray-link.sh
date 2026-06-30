142d9820864b1245dd37cbac0eb050832960cb0a
Kilowat <kadantsev.sergey@gmail.com>
2026-06-30 22:56:13 +0300

    fix

--- xray-link.sh
+++ xray-link.sh
@@ -4,7 +4,7 @@ set -a
 set +a
 
 PUBLIC_KEY=$(docker run --rm teddysun/xray xray x25519 -i "$XRAY_PRIVATE_KEY" | grep "PublicKey" | awk '{print $NF}')
-IP=$(curl -s ifconfig.me)
+IP=$(curl -s -4 ifconfig.me)
 
 echo ""
 echo "vless://${XRAY_UUID}@${IP}:8443?encryption=none&flow=xtls-rprx-vision&security=reality&sni=${XRAY_SERVER_NAME}&fp=chrome&pbk=${PUBLIC_KEY}&sid=${XRAY_SHORT_ID}&type=tcp&headerType=none#${PROXY_NAME}"
